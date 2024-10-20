7611515864e6b7f4b42451d06583de9aa8c7fab0
Matthew Truscott <matthew.a.truscott@gmail.com>
2024-10-20 19:04:36 +0100

    chore: allow dead code

--- src/ray.rs
+++ src/ray.rs
@@ -1,14 +1,17 @@
 use crate::math::vec3::{Point3, Vec3};
 
+#[allow(dead_code)]
 pub struct Ray<'a, 'b> {
     pub origin: &'a Point3,
     pub direction: &'b Vec3,
 }
 
+#[allow(dead_code)]
 impl<'a, 'b> Ray<'a, 'b> {
     pub fn new(origin: &'a Point3, direction: &'b Vec3) -> Self {
         Self { origin, direction }
     }
+
     fn at(&self, t: f64) -> Point3 {
         self.origin + (self.direction * t)
     }
